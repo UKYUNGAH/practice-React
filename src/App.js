27db33dd3bbabe00223aa050a364d11d170fc841
경아 <yookyungah@gyeong-aui-MacBookAir.local>
2024-03-06 23:00:09 +0900

    chore: 리액트 프로젝트 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  let post = '강남 우동 맛집';
+  useState('남자 코트 추천')
+
+  return (
+    <div className="App">
+      <div className='black-nav'>
+        <h4>
+          블로그임
+        </h4>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
