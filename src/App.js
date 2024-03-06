771228e19fc1777d1c1dff83c723c6ccb54d9220
경아 <yookyungah@gyeong-aui-MacBookAir.local>
2024-03-06 23:42:23 +0900

    feat: 스테이트 함수 사용해서 제목 변경 해보기 숙제

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
+/* eslint-disable */
+// warning 메세지 == 경고임 무시해도 됨 없애기
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-// ============ 스테이트[a, b] 의미!
-// a = 스테이트에 보관했던 자료 나옴
-// b = 스테이트 변경 도화주는 함수!
-// 변수는 갑자기 변경되면 자동으로 반영이 안됨 스테이트는 자동 재랜더링이 가능!
-// 변동시 자동으로 html에 반영되게 하고싶을때 쓰면 좋음
 
-
-// ========== let [logo, setLogo] = useState('ReactBlog');
-// 로고같은건 자주 변경되지 않기 때문에 스테이트로 사용 안해도 됨
-// 자주 변경될것같은 html 부분을 스테이트로 만들면 됨!
 
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
 
-
+  
   return (
     <div className="App">
       <div className='black-nav'>
         <h4>ReactBlog</h4>
       </div>
+      <button onClick={()=> {제목변경(['여자 코트 추천', '강남 우동 맛집', '파이썬 독학'])}}>글수정</button>
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={ ()=> { 따봉변경(따봉+1) } }>👍</span>{따봉}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
